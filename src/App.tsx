476f7e22b17839d3470b35ad542799e1fab86aca
Varun Gupta <varungupta1228@gmail.com>
2024-10-02 14:29:36 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { initialPost } from "./utils/data";
+import { CommentType, PostType } from "./utils/types";
+import Comment from "./component/Comment";
+import Post from "./component/Post";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [post, setPost] = useState<PostType>(initialPost);
+
+  const addPostComment = (content: string) => {
+    const newComment: CommentType = {
+      id: Date.now(),
+      username: "User" + Date.now(),
+      userImage: "https://randomuser.me/api/portraits/lego/1.jpg",
+      commentContent: content,
+      timestamp: new Date().getTime(),
+      upvotes: 0,
+      replies: [],
+    };
+
+    setPost({ ...post, comments: [newComment, ...post.comments] });
+  };
+
+  const addReply = (parentId: number, content: string) => {
+    let parentComment: CommentType | null = null;
+
+    const addReplyRecursive = (comments: CommentType[]): CommentType[] => {
+      return comments.map((comment) => {
+        if (comment.id === parentId) {
+          parentComment = {
+            ...comment,
+            replies: [
+              ...comment.replies,
+              {
+                id: Date.now(),
+                username: "User" + Date.now(),
+                userImage: "https://randomuser.me/api/portraits/lego/1.jpg",
+                commentContent: content,
+                timestamp: new Date().getTime(),
+                upvotes: 0,
+                replies: [],
+              },
+            ],
+          };
+          return parentComment;
+        }
+        return {
+          ...comment,
+          replies: addReplyRecursive(comment.replies),
+        };
+      });
+    };
+
+    const updatedComments = addReplyRecursive(post.comments);
+    setPost({ ...post, comments: updatedComments });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-3xl mx-auto mt-8 mb-10 w-full flex flex-col justify-center items-center">
+      <Post post={post} addPostComment={addPostComment} />
+
+      <div className="mt-6 w-full ">
+        {post.comments.map((comment: CommentType) => (
+          <Comment key={comment.id} comment={comment} addReply={addReply} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
